7b890346edbe4e2f853b182905897dd0ae415708
Mikołaj Kamiński <mkaminski321+github@gmail.com>
2017-08-24 19:33:10 +0200

    Fixed .gitignore

--- digitalfilters/sample.py
+++ digitalfilters/sample.py
@@ -2,7 +2,7 @@ class Sample:
     """The Sample represents a single signal sample."""
     def __init__(self, time = 0.0, value = 0.0):
         """Time can't be negative."""
-        if not isinstance(time) or not isinstance(value):
+        if not isinstance(time, float) or not isinstance(value, float):
             raise TypeError("Time and value must be a numbers")
 
         if time < 0:
--- digitalfilters_tests/firfilter_test.py
+++ digitalfilters_tests/firfilter_test.py
@@ -1,16 +1,23 @@
 """Unit test for FIRFilter class"""
 import unittest
-import sys
+import sys, os
 
-sys.path.append("..")
-import digitalfilters as df
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))  
+from digitalfilters import Sample
+from digitalfilters import FIRFilter
 
 class FIRFilterTest(unittest.TestCase):
     def test(self):
-        f = df.FIRFilter([0.25, 0.25, 0.25, 0.25])
-        f.AddSample(df.Sample(0.0, 0.0))
-        f.AddSample(df.Sample(1.0, 1.0))
-        f.AddSample(df.Sample(2.0, 2.0))
-        f.AddSample(df.Sample(3.0, 4.0))
-        calculatedSample = f.AddSample(df.Sample(4.0, 8.0))
-        self.assertEqual(3.75, calculatedSample.val)
+        f = FIRFilter([0.25, 0.25, 0.25, 0.25])
+        f.AddSample(Sample(0.0, 0.0))
+        f.AddSample(Sample(1.0, 1.0))
+        f.AddSample(Sample(2.0, 2.0))
+        f.AddSample(Sample(3.0, 4.0))
+        f.AddSample(Sample(3.0, 8.0))
+        calculatedSample = f.AddSample(Sample(4.0, 2.0))
+        print(calculatedSample.val)
+        self.assertEqual(4.0, calculatedSample.val)
+
+
+if __name__ == "__main__":
+    unittest.main()
